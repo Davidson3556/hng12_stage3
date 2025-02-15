481204c9db4a13ca9547271ca68b1c05aedea81e
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2025-02-15 23:47:57 +0100

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,22 @@
 import type { Metadata } from "next";
-import { Roboto,Road_Rage, Alatsi } from "next/font/google";
-import localFont from 'next/font/local';
+import { Roboto, Road_Rage, Alatsi } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-
 const roboto = Roboto({
-  weight: ['100','300', '700'],
+  weight: ["100", "300", "700"],
   variable: "--font-roboto",
   subsets: ["latin"],
 });
 const road_rage = Road_Rage({
-  weight: '400',
+  weight: "400",
   variable: "--font-road_rage",
   subsets: ["latin"],
 });
 const alatsi = Alatsi({
-  weight: '400',
-  subsets: ['latin'],
-  variable: '--font-alatsi', 
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-alatsi",
 });
 // const jejuMyeongjo = localFont({
 //   src: [
@@ -26,9 +25,9 @@ const alatsi = Alatsi({
 //       weight: '400',
 //       style: 'normal',
 //     },
-   
+
 //   ],
-//   variable: '--font-jejuMyeongjo', 
+//   variable: '--font-jejuMyeongjo',
 // });
 
 export const metadata: Metadata = {
@@ -42,7 +41,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-<html lang="en" className={`${road_rage.variable} ${roboto.variable} ${alatsi.variable}`}>
+    <html
+      lang="en"
+      className={`${road_rage.variable} ${roboto.variable} ${alatsi.variable}`}
+    >
       <body className="min-h-screen bg-[#02191D] f text-[#FAFAFA] antialiased font-roboto">
         {children}
       </body>
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-} from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useTicketStore } from "@/store";
 
